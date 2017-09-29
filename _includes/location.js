f9560a58308440341bacdcc785becf5d975b30a8
Job Chong <jobchong@gmail.com>
2017-09-29 21:29:43 +0800

    make sure redirects appear on all pages

---
+++ _includes/location.js
@@ -0,0 +1,10 @@
+// move to https
+if (location.protocol != 'https:')
+    {
+	location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+    }
+
+// redirect www.legalese.com, and legalese.github.io domains
+if( window.location.hostname != "legalese.com" ) {
+    window.location.replace("https://legalese.com" + window.location.pathname );
+}
