8009f276ddd0701ed88bb5bc75f1b740ef75271d
Job Chong <jobchong@gmail.com>
2019-05-14 12:30:59 +0800

    automated testing for broken links

---
+++ test.js
@@ -0,0 +1,25 @@
+const blc = require('broken-link-checker')
+
+console.log('Known false positives:')
+console.log('Jstor links, NPR links, Imperial links')
+
+
+let acc = []
+
+const siteChecker = new blc.SiteChecker(acc, {
+  link: function(result, customData) {
+    if (result.broken) {
+      console.error(`${result.url.original} is broken because of ${result.brokenReason}`)
+      console.error(`Found on ${result.base.original}`)
+      acc.push(result)
+    }
+  },
+  end: function() {
+    console.log('Finished checks')
+    console.log(`There are a grand total of ${acc.length} broken links.`)
+    console.log(`Please review the log below and fix them as necessary.`)
+    console.log(`${acc.map(a=> `${a.url.original} in ${a.base.original}`)}`)
+  }
+})
+
+siteChecker.enqueue('http://localhost', acc)
