20901c23a6b49bbfd85bc1738acff21f38ef0c60
Thomas Gorissen <thomas.gorissen@me.com>
2026-01-07 10:24:27 +0800

    Allow "none" for footer column

--- src/interfaces/external-link.ts
+++ src/interfaces/external-link.ts
@@ -2,7 +2,7 @@ export type ExternalLink = {
   slug: string;
   title: string;
   url: string;
-  footerColumn: "company" | "resources" | "contact";
+  footerColumn: "company" | "resources" | "contact" | "none";
   order?: number;
   description?: string;
 };
