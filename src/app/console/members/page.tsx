b77bab95692f9a8de56c1514d2d0d4851be0bec0
Thomas Gorissen <thomas.gorissen@me.com>
2026-03-26 23:26:54 +0800

    Add WorkOS domain verification widget to members tab

--- src/app/console/members/page.tsx
+++ src/app/console/members/page.tsx
@@ -1,12 +1,24 @@
 "use client";
 
 import { WidgetPage } from "../console-utils";
+import { useConsole } from "../console-context";
 import { UsersManagement } from "@workos-inc/widgets/users-management";
+import { AdminPortalDomainVerification } from "@workos-inc/widgets/admin-portal-domain-verification";
 
 export default function MembersPage() {
+  const { session } = useConsole();
+  const canManageDomains = session?.permissions.includes("widgets:domain-verification:manage") ?? false;
+
   return (
-    <WidgetPage scope="widgets:users-table:manage">
-      {(token) => <UsersManagement authToken={token} />}
-    </WidgetPage>
+    <div className="space-y-8">
+      <WidgetPage scope="widgets:users-table:manage">
+        {(token) => <UsersManagement authToken={token} />}
+      </WidgetPage>
+      {canManageDomains && (
+        <WidgetPage scope="widgets:domain-verification:manage">
+          {(token) => <AdminPortalDomainVerification authToken={token} />}
+        </WidgetPage>
+      )}
+    </div>
   );
 }
